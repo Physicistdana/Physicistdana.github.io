fe551fcc761b55ace45321b38a41ed88e84074ee
Physicistdana <d.abdallah@wayne.edu>
2026-05-13 05:22:54 +0000

    Deploying to gh-pages from @ Physicistdana/physicistdana.github.io@8376dad0ffd9d0410c5eb5a697f20342867c42c2 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-dana-abdallah",
-    title: "Dana Abdallah",
+    id: "nav-about",
+    title: "About",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
@@ -16,8 +16,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/Research/";
           },
-        },{id: "nav-dana-abdallah",
-          title: "Dana Abdallah",
+        },{id: "nav-about",
+          title: "About",
           description: "",
           section: "Navigation",
           handler: () => {
